e5f2797e7e3657304fed540648d6b52671867ca7
nopenothinghere <nopenothinghere@proton.me>
2023-08-16 03:16:55 -0400

    Works on my machine /s

--- bin/decrypt_secrets.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-gpg --batch --use-agent --decrypt ssh.conf.gpg > ~/.ssh/config
-gpg --batch --use-agent --decrypt vault.gpg > ~/.vault
-gpg --batch --use-agent --decrypt gpg.conf.gpg > ~/.gnupg/gpg.conf
-gpg --batch --use-agent --decrypt hosts.gpg > ~/hosts
--- setup.sh
+++ setup.sh
@@ -7,17 +7,14 @@ mkdir -p ~/.gnupg/
 mkdir -p ~/.ssh/
 mkdir -p ~/.local/share/nvim/
 
-./bin/decrypt_secrets.sh
+./bin/open_the_vault.sh
+gpg --batch --use-agent --decrypt ssh.conf.gpg > ~/.ssh/config
+gpg --batch --use-agent --decrypt hosts.gpg > ~/hosts
+gpg --batch --use-agent --decrypt gpg.conf.gpg > ~/.gnupg/gpg.conf
 
 cp .gitconfig ~
-cp .tmux.conf ~
 cp .vimrc ~
-cp -r .config/* ~/.config
 cp -r .vim/* ~/.vim
 cp python-argcomplete ~/.bash_completion.d
 cp .bashrc ~
-cp .zshrc ~/.zshrc
-cp .zshenv ~/.zshenv
-chmod +x .vault
-cp .vault ~
 cp names.txt ~
